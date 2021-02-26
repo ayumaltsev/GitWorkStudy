324677053942178bf0b0093478ba12fe35555294
Anton Yu. Maltsev <a.maltsev@kpi.ua>
2021-02-26 12:14:05 +0200

    Add Person class

---
+++ src/main/java/org/example/my/Person.java
@@ -0,0 +1,36 @@
+package org.example.my;
+
+public class Person extends BaseEntity{
+    private String firtsName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firtsName, String lastName) {
+        this.firtsName = firtsName;
+        this.lastName = lastName;
+    }
+
+    public Person(Long id, String firtsName, String lastName) {
+        super(id);
+        this.firtsName = firtsName;
+        this.lastName = lastName;
+    }
+
+    public String getFirtsName() {
+        return firtsName;
+    }
+
+    public void setFirtsName(String firtsName) {
+        this.firtsName = firtsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
